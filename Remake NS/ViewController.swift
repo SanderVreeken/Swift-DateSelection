b3af1a9c25cc567f4d2e5b92bf6f17a42be4063b
Alexander Frederik Vreeken <saf.vreeken@gmail.com>
2019-04-03 20:43:10 +0200

    Initial Commit

---
+++ Remake NS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remake NS
+//
+//  Created by Alexander Frederik Vreeken on 03/04/2019.
+//  Copyright © 2019 Alexander Frederik Vreeken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
